6c1a9d8b86b5329199544a1769a0a1725f8a4c58
Dayle <daylevillanda@gmail.com>
2020-04-16 14:50:43 +0800

    CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,31 @@
 class ArticlesController < ApplicationController
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = 'Article was successfully updated'
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = 'Article was successfully created'
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def new
+    @article = Article.new
   end
 
   def index
