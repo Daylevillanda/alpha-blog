f392add1fd26b10433dfb144b0286329fd999ab8
Dayle <daylevillanda@gmail.com>
2020-04-17 11:32:53 +0800

    finish basic

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,32 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = 'Article was successfully updated'
       redirect_to @article
     else
       render 'edit'
     end
   end
-  
-  def edit
-    @article = Article.find(params[:id])
-  end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = 'Article was successfully created'
       redirect_to @article
@@ -24,16 +35,17 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def new
-    @article = Article.new
+  def destroy
+    @article.destroy
+    flash[:notice] = 'Article was successfully deleted'
+    redirect_to articles_path
   end
 
-  def index
-    @articles = Article.all
-  end
-
-  def show
-    # byebug
-    @article = Article.find(params[:id])
-  end
+  private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 end
